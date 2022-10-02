8d250246240c827f62744013dad999e5f13a4720
Kidprogramer1039 <98392401+Kidprogramer1039@users.noreply.github.com>
2022-10-02 06:45:05 +0000

    [feature] 로그인 페이지 디자인 수정 #26

--- frontend/mili-meet/src/pages/login.tsx
+++ frontend/mili-meet/src/pages/login.tsx
@@ -1,21 +1,31 @@
 import { Button } from '@mui/material';
 import { TextField } from '@mui/material';
-import { styles } from './login.module.css';
+import styles from './login.module.css';
+import { Box } from '@mui/material';
 
-function index() {
+function login() {
     return (
         <>
-            <h1>Mili-Meet</h1>
+          <Box
+              sx={{
+                marginTop: 15,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+          >
+            <h1 className={styles.title}>Mili-Meet</h1>
             <div className= {styles.input}>
-              <TextField id="Login" label="Username" variant="outlined"></TextField>
-              <TextField id="Login" label="Password" variant="outlined"></TextField>
+              <TextField id="userID" label="id" variant="outlined" margin="normal" fullWidth/>
+              <TextField id="password" label="Password" variant="outlined" type='password' margin="normal" fullWidth/>
             </div>
             <div className={styles.button}>
-              <Button variant="contained">로그인</Button>
-              <Button variant="contained">회원가입</Button>
+              <Button variant="contained" className="loginPageButton" size="large" sx={{ mt:3, pl:9, pr:9, pt:2, pb:2 }}>로그인</Button>
+              <Button variant="contained" className="loginPageButton" size="large" sx={{ mt:3, pl:9, pr:9, pt:2, pb:2 }}>회원가입</Button>
             </div>
+          </Box>
         </>
       )
     }
   
-  export default index;
+  export default login;
