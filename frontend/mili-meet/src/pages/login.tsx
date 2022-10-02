ee0ecc641d7e0ae58f95f061cd87e193814a9c7f
Kidprogramer1039 <98392401+Kidprogramer1039@users.noreply.github.com>
2022-10-02 05:45:25 +0000

    [feature] 로그인 페이지 문서 추가 #26

--- frontend/mili-meet/src/pages/login.tsx
+++ frontend/mili-meet/src/pages/login.tsx
@@ -1,13 +1,19 @@
 import { Button } from '@mui/material';
 import { TextField } from '@mui/material';
+import { styles } from './login.module.css';
+
 function index() {
     return (
         <>
-            <h1>Mili-Meet</h1> 
-            <TextField id="Login" label="Username" variant="outlined"></TextField>
-            <TextField id="Login" label="Password" variant="outlined"></TextField>
-            <Button variant="contained">로그인</Button>
-            <Button variant="contained">회원가입</Button>
+            <h1>Mili-Meet</h1>
+            <div className= {styles.input}>
+              <TextField id="Login" label="Username" variant="outlined"></TextField>
+              <TextField id="Login" label="Password" variant="outlined"></TextField>
+            </div>
+            <div className={styles.button}>
+              <Button variant="contained">로그인</Button>
+              <Button variant="contained">회원가입</Button>
+            </div>
         </>
       )
     }
