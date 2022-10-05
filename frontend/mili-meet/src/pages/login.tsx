a11593972bb3b8718c0ea2f38c524374b32f6af2
Kidprogramer1039 <98392401+Kidprogramer1039@users.noreply.github.com>
2022-10-05 09:41:19 +0000

    [feature] 로그인 페이지 수정 #26

--- frontend/mili-meet/src/pages/login.tsx
+++ frontend/mili-meet/src/pages/login.tsx
@@ -1,31 +1,60 @@
-import { Button } from '@mui/material';
-import { TextField } from '@mui/material';
-import styles from './login.module.css';
-import { Box } from '@mui/material';
+import styles from "./login.module.css";
+import { Box } from "@mui/material";
 
+// 로그인 화면
 function login() {
-    return (
-        <>
-          <Box
-              sx={{
-                marginTop: 15,
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-              }}
+  return (
+    <>
+      <Box
+        sx={{
+          marginTop: 15,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <h1 className={styles.title}>Mili-Meet</h1>
+        <div className={styles.input}>
+          <TextField
+            id="userID"
+            label="ID"
+            variant="outlined"
+            margin="normal"
+            fullWidth
+          />
+          <TextField
+            id="password"
+            label="Password"
+            variant="outlined"
+            type="password"
+            margin="normal"
+            fullWidth
+          />
+        </div>
+        <div className={styles.button}>
+          <Button
+            variant="contained"
+            className="loginPageButton"
+            size="large"
+            sx={{ mt: 3, pl: 9, pr: 9, pt: 2, pb: 2 }}
+            type="submit"
           >
-            <h1 className={styles.title}>Mili-Meet</h1>
-            <div className= {styles.input}>
-              <TextField id="userID" label="id" variant="outlined" margin="normal" fullWidth/>
-              <TextField id="password" label="Password" variant="outlined" type='password' margin="normal" fullWidth/>
-            </div>
-            <div className={styles.button}>
-              <Button variant="contained" className="loginPageButton" size="large" sx={{ mt:3, pl:9, pr:9, pt:2, pb:2 }}>로그인</Button>
-              <Button variant="contained" className="loginPageButton" size="large" sx={{ mt:3, pl:9, pr:9, pt:2, pb:2 }}>회원가입</Button>
-            </div>
-          </Box>
-        </>
-      )
-    }
-  
-  export default login;
+            로그인
+          </Button>
+          <Button
+            href="./register"
+            variant="contained"
+            className="registerPageButton"
+            size="large"
+            sx={{ mt: 3, pl: 9, pr: 9, pt: 2, pb: 2 }}
+            type="submit"
+          >
+            회원가입
+          </Button>
+        </div>
+      </Box>
+    </>
+  );
+}
+
+export default login;
