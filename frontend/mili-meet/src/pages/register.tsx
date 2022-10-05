d0aa11a7bb62fc5f1abdab1d670688b91f4be29d
Kidprogramer1039 <98392401+Kidprogramer1039@users.noreply.github.com>
2022-10-05 09:41:19 +0000

    [feature] 로그인 페이지 수정 #26

--- frontend/mili-meet/src/pages/register.tsx
+++ frontend/mili-meet/src/pages/register.tsx
@@ -1,31 +1,59 @@
-import { Button } from '@mui/material';
-import { TextField } from '@mui/material';
-import styles from './register.module.css';
-import { Box } from '@mui/material';
+import { Button } from "@mui/material";
+import { TextField } from "@mui/material";
+import styles from "./register.module.css";
+import { Box } from "@mui/material";
 
-function login() {
-    return (
-        <>
-          <Box
-              sx={{
-                marginTop: 15,
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-              }}
+//회원가입 화면
+function Register() {
+  return (
+    <>
+      <Box
+        sx={{
+          marginTop: 15,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <h1 className={styles.title}>Mili-Meet</h1>
+        <div className={styles.input}>
+          <TextField
+            id="email"
+            label="Email"
+            variant="outlined"
+            margin="normal"
+            fullWidth
+          />
+          <TextField
+            id="userId"
+            label="ID"
+            variant="outlined"
+            margin="normal"
+            fullWidth
+          />
+          <TextField
+            id="password"
+            label="Password"
+            variant="outlined"
+            type="password"
+            margin="normal"
+            fullWidth
+          />
+        </div>
+        <div className={styles.button}>
+          <Button
+            href="./login"
+            variant="contained"
+            className="loginPageButton"
+            size="large"
+            sx={{ mt: 3, pl: 34, pr: 34, pt: 2, pb: 2 }}
           >
-            <h1 className={styles.title}>Mili-Meet</h1>
-            <div className= {styles.input}>
-              <TextField id="email" label="Email" variant="outlined" margin="normal" fullWidth/>
-              <TextField id="userId" label="ID" variant="outlined" margin="normal" fullWidth/>
-              <TextField id="password" label="Password" variant="outlined" type='password' margin="normal" fullWidth/>
-            </div>
-            <div className={styles.button}>
-              <Button variant="contained" className="loginPageButton" size="large" sx={{ mt:3, pl:34, pr:34, pt:2, pb:2 }}>회원가입</Button>
-            </div>
-          </Box>
-        </>
-      )
-    }
-  
-  export default login;
+            회원가입
+          </Button>
+        </div>
+      </Box>
+    </>
+  );
+}
+
+export default Register;
