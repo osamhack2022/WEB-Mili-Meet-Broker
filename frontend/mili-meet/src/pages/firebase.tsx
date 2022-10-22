82b2edd5e7c853872b65c7305185da4a127fe08f
Kidprogramer1039 <98392401+Kidprogramer1039@users.noreply.github.com>
2022-10-22 08:00:27 +0000

    [feature] 회원가입 페이지 기능 구현

---
+++ frontend/mili-meet/src/pages/firebase.tsx
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCpZjmaCyjQGJnuYIE5PeUK9SpmWzKpG-8",
+  authDomain: "mili-meet.firebaseapp.com",
+  databaseURL: "https://mili-meet-default-rtdb.firebaseio.com",
+  projectId: "mili-meet",
+  storageBucket: "mili-meet.appspot.com",
+  messagingSenderId: "323599206721",
+  appId: "1:323599206721:web:045e2d822eb69a4175b9c0",
+  measurementId: "G-6JW0FGGXEH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export default app;
