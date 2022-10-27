957ff37a2affa95cf51e67759276ae0e27dbb00c
Kidprogramer1039 <98392401+Kidprogramer1039@users.noreply.github.com>
2022-10-27 09:47:45 +0000

    [feature] 로그인 페이지 기능 구현 중

--- frontend/mili-meet/src/pages/index.tsx
+++ frontend/mili-meet/src/pages/index.tsx
@@ -1,9 +1,17 @@
-import Login from "./loginPage";
-
+import { ReactKeycloakProvider } from '@react-keycloak/web';
+// 따로 정의한 파일이다. keycloak instance, option 들을 가져온다.
+import keycloak, { initOptions, onKeycloakEvent } from '../../../../keycloak/keycloak';
+import RoomCode from "./roomCode";
 function Index() {
   return (
   <>
-  <Login />
+    <ReactKeycloakProvider
+    authClient={keycloak}
+    initOptions={initOptions}
+    onEvent={onKeycloakEvent}
+    >
+    <RoomCode />
+  </ReactKeycloakProvider>
   </>
   )
 }
