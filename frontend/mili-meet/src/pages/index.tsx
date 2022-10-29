d4e1a5c8be73b59dc09ff16fb8495453e6dc8598
Kidprogramer1039 <98392401+Kidprogramer1039@users.noreply.github.com>
2022-10-29 03:03:41 +0000

    [feature] 로그인 화 구현 중

--- frontend/mili-meet/src/pages/index.tsx
+++ frontend/mili-meet/src/pages/index.tsx
@@ -1,17 +1,16 @@
+import React from 'react';
 import { ReactKeycloakProvider } from '@react-keycloak/web';
 // 따로 정의한 파일이다. keycloak instance, option 들을 가져온다.
-import keycloak, { initOptions, onKeycloakEvent } from '../../../../keycloak/keycloak';
+import keycloak from '../../../../keycloak/keycloak';
 import RoomCode from "./roomCode";
 function Index() {
   return (
   <>
-    <ReactKeycloakProvider
-    authClient={keycloak}
-    initOptions={initOptions}
-    onEvent={onKeycloakEvent}
-    >
+  <ReactKeycloakProvider
+  initOptions={{ onLoad: 'login-required'}}
+  authClient={keycloak}>
     <RoomCode />
-  </ReactKeycloakProvider>
+    </ReactKeycloakProvider>
   </>
   )
 }
