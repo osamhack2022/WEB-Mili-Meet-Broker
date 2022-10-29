8cdfdc4fff3aceea2336daf2c545696be017969b
Kidprogramer1039 <98392401+Kidprogramer1039@users.noreply.github.com>
2022-10-29 03:03:41 +0000

    [feature] 로그인 화 구현 중

--- frontend/mili-meet/src/pages/loginPage.tsx
+++ frontend/mili-meet/src/pages/loginPage.tsx
@@ -4,6 +4,7 @@ import { Box } from "@mui/material";
 import Link from "next/link";
 import { SetStateAction, useState } from 'react';
 import { doc, getDoc } from "firebase/firestore";
+import keycloak from '../../../../keycloak/keycloak';
 
 // 로그인 화면
 function Login() {
@@ -72,10 +73,7 @@ function Login() {
                 className="loginPageButton"
                 size="large"
                 sx={{ mt: 3, pl: 9, pr: 9, pt: 2, pb: 2 }}
-                onClick={() => getUser({
-                  id : {userId},
-                  password : {Password}
-                })}
+                onClick={() => keycloak.login()}
               >
                 로그인
               </Button>
--- keycloak/keycloak.ts
+++ keycloak/keycloak.ts
@@ -1,39 +1,10 @@
-import Keycloak from 'keycloak-js';
-const KEYCLOAK_REALM_NAME = 'Mili-meet' // keycloak realm 
-const KEYCLOAK_CLIENT_ID = 'public-client'; // 사용할 client 
-const KEYCLOAK_URL = 'osamhack2022-v2-web-mili-meet-broker-gv5wqp76qjx2v66p-8080.preview.app.github.dev'; // keycloak url
+import Keycloak from "keycloak-js";
 
 // 위에 설정한 realm, client_id, url 로 keycloak instance 를 생성한다.
-const keycloak: Keycloak.KeycloakInstance = Keycloak({
-  realm: KEYCLOAK_REALM_NAME,
-  clientId: KEYCLOAK_CLIENT_ID,
-  url: KEYCLOAK_URL
-});
-
-// initOption 지정
-export const initOptions = {
-  onLoad: 'login-required',
-  checkLoginIframe: false,
-};
-
-// keycloak Event 를 보기 위한 함수 정의
-// keycloak provider 의 onEvent 에 넣어준다.
-export const onKeycloakEvent = (event: any, error: any) => {
-  console.log('keycloak event ', event, error);
-  switch (event) {
-    case 'onAuthLogout':
-      keycloak.logout();
-      break;
-    case 'onAuthRefreshError':
-      keycloak.logout();
-      break;
-    case 'onAuthRefreshSuccess':
-         console.log('auth token:  ' + keycloak.token);
-      console.log('refresh token:  ' + keycloak.refreshToken);
-      break;
-    default:
-      break;
-  }
-};
+let keycloak = new Keycloak({
+  url: "https://osamhack2022-v2-web-mili-meet-broker-gv5wqp76qjx2v66p-3000.preview.app.github.dev/",
+  realm: "Mili-meet",
+  clientId: "public-client",
+ });
 
 export default keycloak;
