f119a0cdc3ec428dcf5dcabc63058d190d3c41fb
JunSangYu <35032401+stupidJoon@users.noreply.github.com>
2022-10-30 01:37:46 +0000

    [feature] env 수정

--- frontend/mili-meet/src/pages/api/auth/[...nextauth].ts
+++ frontend/mili-meet/src/pages/api/auth/[...nextauth].ts
@@ -5,10 +5,10 @@ export const authOptions: NextAuthOptions = {
   providers: [
     KeycloakProvider({
       clientId: 'frontend-client',
-      clientSecret: '6DT9hwLgXVzxpOp9So9JCJ2dtxPKicOS',
-      issuer: 'https://osamhack2022-web-mili-meet-broker-7rrgrq5695q2pp9-8080.preview.app.github.dev/realms/mili-meet',
+      clientSecret: process.env.KEYCLAOK_CLIENTSECRET ?? '',
+      issuer: process.env.KEYCLOAK_ISSUER ?? 'https://osamhack2022-web-mili-meet-broker-7rrgrq5695q2pp9-8080.preview.app.github.dev/realms/mili-meet',
     })
   ]
 }
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
